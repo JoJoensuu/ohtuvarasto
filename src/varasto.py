893b97e06f699bca62d570dc29cbc6823cd830c9
Joni Joensuu <joni.joens@gmail.com>
2022-11-10 16:37:36 +0200

    viikko 2 tehtävä 4

--- src/varasto.py
+++ src/varasto.py
@@ -1,6 +1,10 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        self.tilavuus = tilavuus * tilavuus / tilavuus
+        if self.tilavuus_ok(tilavuus):
+            self.tilavuus = tilavuus
+        else:
+            # virheellinen, nollataan
+            self.tilavuus = 0.0
 
         if alku_saldo < 0.0:
             # virheellinen, nollataan
